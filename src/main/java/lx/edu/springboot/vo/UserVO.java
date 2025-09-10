8ff63af50c034f12751a74073523f8d2dc6b42ac
ji-gaem2 <denver6761@gmail.com>
2025-09-10 21:23:52 +0900

    add UserVO

---
+++ src/main/java/lx/edu/springboot/vo/UserVO.java
@@ -0,0 +1,28 @@
+package lx.edu.springboot.vo;
+
+public class UserVO {
+	
+	String userId;
+	String userPassword;
+	String userName;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
