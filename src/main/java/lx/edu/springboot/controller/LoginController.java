329942621f158327a41231d92b8e931df55dd349
ji-gaem2 <denver6761@gmail.com>
2025-09-12 17:20:52 +0900

    0912 중간취합

--- src/main/java/lx/edu/springboot/controller/LoginController.java
+++ src/main/java/lx/edu/springboot/controller/LoginController.java
@@ -1,10 +1,13 @@
 
 // 첫 생성
+// 로그인, 회원가입 추가
 
 package lx.edu.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -22,20 +25,36 @@ public class LoginController {
 	public String insertLoginDB(LoginVO vo) throws Exception { //Spring에서는 이렇게 사용 가능!
 		System.out.println(vo);
 		dao.insertLoginDB(vo);
-		return "redirect:/home.do";
+		return "login";
 	}
-
 	
 	@RequestMapping("/updateLogin.do")
 	public String updateLoginDB(LoginVO vo) throws Exception {
 		dao.updateLoginDB(vo);
-		return "redirect:/home.do";
+		return "redirect:/login";
 	}
 	
 	@RequestMapping("/home") 
-		public  String goToHome() {
-			return "home";
-		}
+	public String goToHome() {
+		return "home";
+	}
+
+	@RequestMapping("/doLogin") 
+	public String doLogin() {
+		return "home";
+	}
+
+	@GetMapping("/login")
+	public String goToLogin(Model model) {
+		model.addAttribute("vo", new LoginVO()); // 빈 vo 객체 전달
+		return "login";
+	}
+
+	@GetMapping("/register")
+	public String goToRegister(Model model) {
+		model.addAttribute("vo", new LoginVO());
+		return "register";
+	}
 
 	
 
