3d533a68b6b8780821da7314f08b6833be7db750
minmin721 <lghpds000@naver.com>
2025-09-15 09:40:17 +0900

    잘못올린거

--- src/main/java/lx/edu/springboot/controller/TravelController.java
+++ src/main/java/lx/edu/springboot/controller/TravelController.java
@@ -1,16 +1,18 @@
-package lx.myproject.controller;
+package lx.edu.springboot.controller;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import lx.myproject.dao.TravelDAO;
-import lx.myproject.vo.TravelVO;
+import lx.edu.springboot.dao.TravelDAO;
+import lx.edu.springboot.vo.TravelVO;
 
 @Controller
 public class TravelController {
@@ -26,7 +28,7 @@ public class TravelController {
 		
 		//TravelVO mf = dao.getMaxFiveByResultFateId(resultFateID);
 		//(유저네임 추가시)TravelVO un = dao.getUserNameByResultFateId(resultFateID);
-		String maxFive = "목";
+		String maxFive = "화";
 		List<TravelVO> recoList = dao.getRecommendedByMaxFive(maxFive);
 		String bad = dao.getBadByMaxFive(maxFive);
 		List<TravelVO> avList = dao.getAvoidByBad(bad);
@@ -35,4 +37,25 @@ public class TravelController {
 		result.setViewName("travel");
 		return result;
 	}
+	
+	@Value("${maps.api.key:}")
+	private String mapsApiKey;
+	
+	@RequestMapping("travel/map.do")
+	public ModelAndView map(@RequestParam("travelId") int travelId) throws Exception {
+		ModelAndView result = new ModelAndView();
+		TravelVO vo = dao.selectTravelDB(travelId);
+		
+		result.addObject("travel", vo);
+		result.addObject("mapsApiKey", mapsApiKey);
+		result.setViewName("travel_map");
+		return result;
+	}
+	
+	@RequestMapping("/conversation_select.do")
+	public String conversation() {
+		return "conversation_select";
+	}
+	
 }
+
