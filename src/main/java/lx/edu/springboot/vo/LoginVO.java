3200bf915251cc6093150731292a562e1eeb1dbf
ji-gaem2 <denver6761@gmail.com>
2025-09-15 11:24:50 +0900

    오전 머지 전

--- src/main/java/lx/edu/springboot/vo/LoginVO.java
+++ src/main/java/lx/edu/springboot/vo/LoginVO.java
@@ -13,24 +13,31 @@ public class LoginVO {
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getUserPassword() {
 		return userPassword;
 	}
+
 	public void setUserPassword(String userPassword) {
 		this.userPassword = userPassword;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+	
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
