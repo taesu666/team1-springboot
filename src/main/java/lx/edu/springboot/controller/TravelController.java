a662968744a9b477908b99f275327dffe2273ad4
taesu666 <wmiso.j@gmail.com>
2025-09-15 09:45:48 +0900

    9-14일 지겸이꺼 뺴고 싹다 마지

--- src/main/java/lx/edu/springboot/controller/TravelController.java
+++ src/main/java/lx/edu/springboot/controller/TravelController.java
@@ -3,6 +3,7 @@ package lx.edu.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,13 +21,13 @@ public class TravelController {
 	@Autowired
 	TravelDAO dao;
 	
-	@RequestMapping("/travel.do")
+	@RequestMapping("travel.do")
 	public ModelAndView travel(/*@RequestParam("maxFive") String maxFive*/) throws Exception {
 		ModelAndView result = new ModelAndView(); 
 		
 		//TravelVO mf = dao.getMaxFiveByResultFateId(resultFateID);
 		//(유저네임 추가시)TravelVO un = dao.getUserNameByResultFateId(resultFateID);
-		String maxFive = "목";
+		String maxFive = "화";
 		List<TravelVO> recoList = dao.getRecommendedByMaxFive(maxFive);
 		String bad = dao.getBadByMaxFive(maxFive);
 		List<TravelVO> avList = dao.getAvoidByBad(bad);
@@ -35,4 +36,24 @@ public class TravelController {
 		result.setViewName("travel");
 		return result;
 	}
+	
+	@Value("${maps.api.key:}")
+	private String mapsApiKey;
+	
+	@RequestMapping("travel/map.do")
+	public ModelAndView map(@RequestParam("travelId") int travelId) throws Exception {
+		ModelAndView result = new ModelAndView();
+		TravelVO vo = dao.selectTravelDB(travelId);
+		
+		result.addObject("travel", vo);
+		result.addObject("mapsApiKey", mapsApiKey);
+		result.setViewName("travel_map");
+		return result;
+	}
+	
+	@RequestMapping("/conversation_select.do")
+	public String conversation() {
+		return "conversation_select";
+	}
+	
 }
